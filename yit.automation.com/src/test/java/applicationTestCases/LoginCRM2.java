14a0ea806d7b6af56aa36704f23914fefdc97b34
admin <admin@admin-PC>
2018-02-10 21:27:42 +0530

    Create second test case
    Signed-off-by: admin <admin@admin-PC>

---
+++ yit.automation.com/src/test/java/applicationTestCases/LoginCRM2.java
@@ -0,0 +1,32 @@
+package applicationTestCases;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import applicationFactory.DataProviderFactory;
+import applicationPages.LoginPage;
+import helper.BaseClass;
+import helper.Utility;
+
+public class LoginCRM2 extends BaseClass {
+	
+
+	@Test
+	public void loginAdmin()
+	{
+		logger=report.startTest("Login, Admin");
+	LoginPage login = PageFactory.initElements(driver, LoginPage.class);
+		login.enterUsername(DataProviderFactory.getExcel().getStringData("Login", 0, 0));
+		login.enterPassword(DataProviderFactory.getExcel().getStringData("Login", 0, 1));
+		login.clickOnLoginButton();
+	
+	}
+	
+	
+	
+}
